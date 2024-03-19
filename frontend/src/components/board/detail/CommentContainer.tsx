cdc48f52d4029844394751d7ee78c1ab250fe729
좌소연 <thdus981005@naver.com>
2024-03-19 13:14:59 +0900

    Feat(S10P22A203-106) : 게시판 상세 페이지 댓글 구현

---
+++ frontend/src/components/board/detail/CommentContainer.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components';
+import CommentCard from './CommentCard';
+
+import { gray2 } from '../../../assets/styles/palettes';
+
+interface Comments{
+  comments : Comment[];
+}
+
+interface Comment{
+  commentId : number;
+  commentWriterId : number;
+  commentCreatedAt : string;
+  commentWriterNickname : string;
+  commentContent : string;
+}
+
+const Wrapper = styled.div`
+  width : 100%;
+  height: auto;
+`
+
+const CommentHr = styled.hr`
+  background: ${gray2};
+  height:0.5px;
+  border:0;
+`
+
+export default function CommentContainer({comments} : Comments) {
+  return (
+    <Wrapper>
+      {
+        comments && (
+          comments.map((comment)=>(
+            <>
+              <CommentCard
+                commentId={comment.commentId}
+                commentWriterId = {comment.commentWriterId}
+                commentCreatedAt = {comment.commentCreatedAt}
+                commentWriterNickname= {comment.commentWriterNickname}
+                commentContent={comment.commentContent}
+                key = {comment.commentId}
+              />
+              <CommentHr/>
+            </>
+          )) 
+        )
+      }
+    </Wrapper>
+  )
+}
