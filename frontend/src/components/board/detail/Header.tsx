190b2a787bf0702f20bcf0cbc8d8ea2d0c9763e6
pty9714 <pty9714@naver.com>
2024-03-22 10:13:57 +0900

    스캔페이지 개발환경구성

--- frontend/src/components/board/detail/Header.tsx
+++ frontend/src/components/board/detail/Header.tsx
@@ -1,53 +1,51 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
-import BackButton from '../../common/BackButton'
-import IconButton from '../../common/IconButton'
-import ExportIcon from '../../../assets/icons/export.svg';
-import MenuIcon from '../../../assets/icons/dotsThree.svg';
-
+import BackButton from "../../common/BackButton";
+import IconButton from "../../common/IconButton";
+import ExportIcon from "../../../assets/icons/export.svg";
+import MenuIcon from "../../../assets/icons/dotsThree.svg";
 
 const Wrapper = styled.div`
-  position : fixed;
-  top : 0;
+  position: fixed;
+  top: 0;
   left: 0;
 
   background-color: white;
 
-  width : 90%;
-  height : 60px;
+  width: 90%;
+  height: 60px;
 
-  padding : 0 5% 0 5%;
+  padding: 0 5% 0 5%;
 
-  display : flex;
+  display: flex;
   flex-direction: row;
   justify-content: space-between;
 
-  &>div{
-    width : 16%;
-    margin : 5% 0 5% 0;
+  & > div {
+    width: 16%;
+    margin: 5% 0 5% 0;
 
-    display : flex;
+    display: flex;
     justify-content: space-between;
   }
-`
-
+`;
 
 export default function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onClickBackBtn = () => {
-        navigate(-1);
-    }
+  const onClickBackBtn = () => {
+    navigate(-1);
+  };
 
-    return (
-        <Wrapper>
-            <BackButton onClickBtn={onClickBackBtn}></BackButton>
-            <div>
-            <IconButton width = '45%' icon = {ExportIcon}></IconButton>
-            <IconButton width = '45%' icon = {MenuIcon}></IconButton>
-            </div>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <BackButton onClickBtn={onClickBackBtn}></BackButton>
+      <div>
+        <IconButton width="45%" icon={ExportIcon}></IconButton>
+        <IconButton width="45%" icon={MenuIcon}></IconButton>
+      </div>
+    </Wrapper>
+  );
 }
