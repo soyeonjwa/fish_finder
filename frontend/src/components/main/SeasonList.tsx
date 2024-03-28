d686096da8a282c7dff6cc40989f551eac4d8bd1
pty9714 <pty9714@naver.com>
2024-03-28 17:47:32 +0900

    Feat(S10P22A203-216-fe-api-연결-메인페이지) 메인페이지 api 연결(제철 월조회 - 수정해야됨)

--- frontend/src/components/main/SeasonList.tsx
+++ frontend/src/components/main/SeasonList.tsx
@@ -1,127 +1,142 @@
-import React from 'react'
-import styled from 'styled-components'
-import Slider from 'react-slick';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { Button } from '../common/Button';
+import { Button } from "../common/Button";
 
-import dataSet from '../../services/dummy/seasonList.json';
-import { useNavigate } from 'react-router-dom';
-const settings = {
-    // dots : true,
-    infinite : true,
-    speed : 500,
-    slidesToShow : 1,
-    slidesToScroll : 1,
-    autoplay : true,
-    autoplaySpeed : 10000,
-    arrows: false, 
+import { useNavigate } from "react-router-dom";
+
+import { axiosInstance } from "../../services/axios";
+import { AxiosResponse } from "axios";
+
+interface FishData {
+  fishId: number;
+  imgUri: string;
+  name: string;
+  description: string;
 }
 
+const settings = {
+  // dots : true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  arrows: false,
+};
+
 const Wrapper = styled.div`
-    font-family : Pretendard;
-    font-size : 18px;
-    font-weight : 600;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-weight: 600;
 
-    margin : 0% 5% 0% 5%;
-    width : 90%;
-    
-    height : auto;
-    display : flex;
-    flex-direction : column;
-`
+  margin: 0% 5% 0% 5%;
+  width: 90%;
 
+  height: auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 const SlideContent = styled.div`
-    position : relative;
-`
+  position: relative;
+`;
 
 const SeasonFish = styled.img`
-    margin : 3% 0 0 0;
-    width : 100%;
-    height : 175px; 
-    border-radius : 10px;
-    object-fit : cover;
+  margin: 3% 0 0 0;
+  width: 100%;
+  height: 175px;
+  border-radius: 10px;
+  object-fit: cover;
 
-    @media only screen and (max-width: 280px) {
-        height : 130px;
-    }
-`
+  @media only screen and (max-width: 280px) {
+    height: 130px;
+  }
+`;
 
 const OrderButton = styled(Button)`
-    position : absolute;
-    right : 5%;
-    top : 10%;
-`
+  position: absolute;
+  right: 5%;
+  top: 10%;
+`;
 
 const FishComment = styled.div`
-    font-size : 20px;
-    color : white;
-    position: absolute;
-    right : 5%;
-    bottom : 22%;
-`
+  font-size: 20px;
+  color: white;
+  position: absolute;
+  right: 5%;
+  bottom: 22%;
+`;
 
 const FishSubComment = styled(FishComment)`
-    font-size : 14px;
-    color : white;
-    bottom : 10%;
-    font-weight : 300;
-`
+  font-size: 14px;
+  color: white;
+  bottom: 10%;
+  font-weight: 300;
+`;
 
 const GradientOverlay = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-radius : 10px;
-    background: linear-gradient(to bottom, rgba(255,255,255,0), #212121);
-`
-
-
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  background: linear-gradient(to bottom, rgba(255, 255, 255, 0), #212121);
+`;
 
 export default function SeasonList() {
-    const navigate = useNavigate();
-    let order = 0;
+  const navigate = useNavigate();
+  let order = 0;
 
-    const calcOrder = () => {
-        order = order+1;
-        return order;
-    }
+  const calcOrder = () => {
+    order = order + 1;
+    return order;
+  };
 
+  const [dataSet, setDataSet] = React.useState<FishData[]>([]);
 
-    return (
-        <Wrapper>
-            <div>제철 횟감</div>
-            <Slider {...settings}>
-                {
-                    dataSet && (
-                        dataSet.map(data => (
-                            <SlideContent key = {data.id}>
-                                <SeasonFish src = {data.imgUri}></SeasonFish>
-                                <GradientOverlay onClick = {() => {navigate(`info/${data.id}`)}}></GradientOverlay>
-                                <OrderButton
-                                    color = '#FFFFFF'
-                                    width = 'auto'
-                                    height = 'auto'
-                                    fontSize='12px'
-                                    backcolor='rgb(0,0,0,0.5)'
-                                    margin ='0'
-                                    border = '0px'
-                                    padding = '2% 4% 2% 4%'
-                                    cursor = 'none'
-                                >{calcOrder()} / {dataSet.length}</OrderButton>
-                                <FishComment>{data.season}{data.name}</FishComment>
-                                <FishSubComment>{data.description}</FishSubComment>
-                            </SlideContent>
-                            
-                        ))
-                    )
-                }
-            </Slider>
-            
-        </Wrapper>
-    )
+  useEffect(() => {
+    axiosInstance.get("/api/banner").then((res: AxiosResponse) => {
+      setDataSet(res.data.data.fishList);
+    });
+  }, []);
+
+  return (
+    <Wrapper>
+      <div>제철 횟감</div>
+      <Slider {...settings}>
+        {dataSet &&
+          dataSet.map((data) => (
+            <SlideContent key={data.fishId}>
+              <SeasonFish src={data.imgUri}></SeasonFish>
+              <GradientOverlay
+                onClick={() => {
+                  navigate(`info/${data.fishId}`);
+                }}
+              ></GradientOverlay>
+              <OrderButton
+                color="#FFFFFF"
+                width="auto"
+                height="auto"
+                fontSize="12px"
+                backcolor="rgb(0,0,0,0.5)"
+                margin="0"
+                border="0px"
+                padding="2% 4% 2% 4%"
+                cursor="none"
+              >
+                {calcOrder()} / {dataSet.length}
+              </OrderButton>
+              <FishComment>{data.name}</FishComment>
+              <FishSubComment>{data.description}</FishSubComment>
+            </SlideContent>
+          ))}
+      </Slider>
+    </Wrapper>
+  );
 }
