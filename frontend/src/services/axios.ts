fa8bbc4f93d46d5c3788bb487c0d98982c2c014a
좌소연 <thdus981005@naver.com>
2024-03-29 20:03:14 +0900

    Fix(S10P22A203-216) : 빌드 에러 수정

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -3,6 +3,12 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
+
+export const axiosMultipartInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  headers: {
+    'Content-Type': 'multipart/form-data'
+  },})
