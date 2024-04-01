3dd4feb8d429bd629c9a96b100f084275765f642
pty9714 <pty9714@naver.com>
2024-04-02 00:05:15 +0900

    Feat 어종 탐색 안될시 재촬영 텍스트 추가

--- frontend/src/pages/scan/Scan.tsx
+++ frontend/src/pages/scan/Scan.tsx
@@ -73,6 +73,22 @@ const ScanButton = styled.img`
   bottom: 15%;
 `;
 
+const RetakeNotice = styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: White;
+  font-size: 20px;
+  bottom: 30%;
+
+  & > p {
+    margin: 2%;
+  }
+`;
+
 const Info = styled.div`
   display: flex;
   height: 10vh;
@@ -344,6 +360,11 @@ export default function Scan() {
                 }}
               ></ScanBox>
             ))}
+          {photoTaken && boxdata.length === 0 && (
+            <RetakeNotice>
+              <p>어종을 찾을 수 없습니다.</p> <p> 다시 촬영해주세요</p>
+            </RetakeNotice>
+          )}
         </VideoBox>
         <ScanButton
           style={{ display: photoTaken ? "none" : "block" }}
