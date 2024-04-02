6ef095cf2a4f9e399baf5ec755c33eed56b23e63
좌소연 <thdus981005@naver.com>
2024-04-02 20:54:20 +0900

    Fix(S10P22A203-288) : 리뷰 게시물 등록 수정

--- frontend/src/components/board/register/Header.tsx
+++ frontend/src/components/board/register/Header.tsx
@@ -51,21 +51,21 @@ export default function Header({fishDatas} : HeaderProps) {
   const {reviewForms} = reviewFormStore();
   const {postType, title, content, images, reviews} = usePostStore();
   const {setPostType, setTitle, setContent, setImages, setReviews} = usePostStore();
+  const post = {
+    data : {
+      title,
+      content,
+      postType,
+      reviews
+    },
+    images
+  }
 
   const onClickBackBtn = () => {
     navigate("/board");
   };
 
   const onClickSubmitBtn = () => {
-    const post = {
-      data : {
-        title,
-        content,
-        postType,
-        reviews
-      },
-      images
-    }
 
     for(let i=0;i<reviewForms.length;i++){
       if(!fishDatas.has(reviewForms[i].review.name)){
@@ -88,6 +88,10 @@ export default function Header({fishDatas} : HeaderProps) {
 
 
     console.log(post)
+    submitData();
+  };
+
+  const submitData = () => {
     const formData = new FormData();
     const json = JSON.stringify(post.data);
     const blob = new Blob([json], {type : 'application/json'});
@@ -117,9 +121,10 @@ export default function Header({fishDatas} : HeaderProps) {
         navigate(`/board/${board.boardId}`)
       })
       .catch(()=>{
+        console.log(post);
         alert("게시물 등록에 실패했습니다.")
       })
-  };
+  }
 
   return (
     <Wrapper>
