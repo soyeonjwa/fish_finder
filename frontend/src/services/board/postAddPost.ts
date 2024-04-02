534203535dc41d0abf7d7af418876e8b73fd9ea0
좌소연 <thdus981005@naver.com>
2024-04-02 13:19:41 +0900

    Fix(S10P22A203-288) : 게시물 등록 api 연결 수정

--- frontend/src/services/board/postAddPost.ts
+++ frontend/src/services/board/postAddPost.ts
@@ -23,7 +23,6 @@ async function postAddPost(post: {
 
   axiosMultipartInstance.post("/api/board", formData)
     .then((res : AxiosResponse) => {
-      if(res.status !== 200) return -1;
       return res.data.data.boardId;
     })
     .catch(() => {throw new Error("게시글 작성에 실패했습니다")})
